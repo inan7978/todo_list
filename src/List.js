a4a24ba85985fc4740908496abd840125b20b5ec
Ismailov <Iismailov@hospiceatcharlotte.net>
2023-09-18 17:08:24 -0400

    Added a simply key generating method that grabs the last id in the list and increments it by one to the new item

--- src/List.js
+++ src/List.js
@@ -1,14 +1,27 @@
-import { useState } from "react";
+import { useState, useId } from "react";
 
 function List() {
+  const taskId = useId();
   const [list, setList] = useState([
     { id: 0, task: "Do HW", completed: false },
     { id: 1, task: "go to gym", completed: false },
     { id: 2, task: "put gas in the car", completed: true },
   ]);
 
-  function addHandler() {
-    setList([...list, { id: 3, task: "put gas in the car", completed: true }]);
+  function getKey() {
+    return list[list.length - 1].id + 1;
+  }
+
+  function addHandler(e) {
+    e.preventDefault();
+    setList([
+      ...list,
+      {
+        id: getKey(),
+        task: "ttt",
+        completed: true,
+      },
+    ]);
     console.log(list);
   }
 
@@ -19,14 +32,16 @@ function List() {
   return (
     <>
       {mappedItems}
-      <button
-        className="add-btn"
-        onClick={() => {
-          addHandler();
-        }}
-      >
-        Add +
-      </button>
+      <span>
+        <form onSubmit={addHandler}>
+          <label>
+            <input name="taskAdd" type="text" placeholder="Enter task here" />
+          </label>
+          <button type="submit" className="add-btn">
+            Add +
+          </button>
+        </form>
+      </span>
     </>
   );
 }
